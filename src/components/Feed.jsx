a07d413d9355ed560574ad3e61ff41e70b3f0f5e
SoftGen <admin@softgen.ai>
2024-07-11 23:13:44 +0000

    SOFTGEN MAGIC

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,6 +21,7 @@ const PostCard = React.memo(({ post, onAction, onRepost }) => {
   
   return (
     <motion.div
+      key={post.id}
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
